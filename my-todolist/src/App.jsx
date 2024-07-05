01b6949760f24c15953ad8728e6c0f9ed3c1f6df
Maryam Akraiche <akraiche.maryam@gmail.com>
2024-07-05 16:48:00 +0200

    added localstorage

--- my-todolist/src/App.jsx
+++ my-todolist/src/App.jsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header.jsx';
 import Input from './Input.jsx';
 import Todolist from './Todolist.jsx';
 
 
 function App() {
-  const initialTodos = [
-    {title: "Learn React", checked: false},
-    {title: "Be Awesome!", checked: false}
-  ];
+  
+  const initialTodos = () => {
+    const storedTasks = localStorage.getItem('tasks');
+    return storedTasks ? JSON.parse(storedTasks) : [
+      { title: "Learn React", checked: false },
+      { title: "Be Awesome!", checked: false }
+    ];
+  };
 
   const [tasks, setTasks] = useState(initialTodos);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const addTask = (newTask) => {
     if (newTask.trim() !== '') {
